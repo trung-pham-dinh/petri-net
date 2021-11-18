47873c5e8b1d8211736454aaf4b46eec84ba359b
hescul <minh.nguyen207bk@hcmut.edu.vn>
2021-11-18 11:43:47 +0700

    define an example of PetriNet usage

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import PetriNet as PN
+import PetriNet as pn
+
+free = pn.Place(1)
+wait = pn.Place(3)
+busy = pn.Place()
+insd = pn.Place()
+docu = pn.Place()
+done = pn.Place(1)
+
+start  = pn.Transition([free, wait], [busy, insd])
+change = pn.Transition([busy, insd], [docu, done])
+end    = pn.Transition([docu], [free])
+
+petri = pn.PetriNet([start, change, end])
+
